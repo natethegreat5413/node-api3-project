0805305c9589f68d963ff30667a2575fbbc82b89
Nathan Cowley <nathancowley12@gmail.com>
2020-09-03 09:56:19 -0600

    changes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const server = require('./server')
-
-server.listen(8001, () => {
-    console.log('\n** Server Running on localhost:8001 **\n')
-})
-
-
-
-// require('dotenv').config()
-// // code away! ok!
 // const server = require('./server')
 
-// const port = process.env.PORT || 8000
-
-// server.listen(port, () => {
-//     console.log(`\n* Server Running on http://localhost:${port} *\n`)
+// server.listen(8001, () => {
+//     console.log('\n** Server Running on localhost:8001 **\n')
 // })
+
+
+
+require('dotenv').config()
+// code away! ok!
+const server = require('./server')
+
+const port = process.env.PORT || 8000
+
+server.listen(port, () => {
+    console.log(`\n* Server Running on http://localhost:${port} *\n`)
+})
 
